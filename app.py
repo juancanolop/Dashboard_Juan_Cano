7b0bb8fbaf05bc86d5c4d7de7c4c3289aa543a8b
Juan David Cano <juadcanolop@gmail.com>
2025-08-30 21:34:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ st.markdown("""
         display: block;
         padding: 10px 15px;
         margin: 6px 0;
-        background-color: #1557b0;
+        background-color: #07b9d1;
         color: white !important;
         text-decoration: none;
         font-size: 0.95rem;
