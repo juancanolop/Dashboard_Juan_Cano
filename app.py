53d1cfcff9733504cdcfa646981d2140bfd8c303
Juan David Cano <juadcanolop@gmail.com>
2025-08-30 21:41:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def create_navigation_sidebar():
         st.markdown("""
         <div class="logo-container">
             <a href="https://www.juandavidcano.com/" target="_blank">
-                <img src="https://res.cloudinary.com/dmf2pbdlq/image/upload/v1756601724/Profile_zwgeyn.png" 
+                <img src="https://res.cloudinary.com/dmf2pbdlq/image/upload/v1756604466/Untitled_design_y5c8ts.png" 
                      alt="Juan David Cano"
                      style="width: 280px; height: auto; border-radius: 10px; cursor: pointer;">
             </a>
